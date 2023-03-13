fba570e72bc151114e6b9b369534186a79115dd2
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-03-12 21:03:22 -0600

    Actuali

--- Python Backend/type_hints.py
+++ Python Backend/type_hints.py
@@ -4,3 +4,7 @@ varibale_texto: int = "Esto es un texto"
 
 print(varibale_texto)
 print(type(varibale_texto))
+
+"Qué es el tipado dinamico?"
+#El tipado dinamico es cuando asignamos un valor a una variable, pero luego le cambiamos ese valor por otro
+#Como siempre goku le gana
